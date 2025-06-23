4deb109605fc80e88b955ea362acee17e567592a
Argie Mula <sd.argie.mula@gmail.com>
2025-06-24 02:07:22 +0800

    Added Initial File

---
+++ src/main/java/com/sd/server/AnnuityApp1Application.java
@@ -0,0 +1,13 @@
+package com.sd.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnuityApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnuityApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sd/server/AnnuityApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnuityApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
